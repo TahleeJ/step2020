e5957f2aa695b692378174f67325c22920bf42e5
TahleeJ <tahlee.jaynes@gmail.com>
2020-06-10 21:51:08 +0000

    Protobuf version 2

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -22,12 +22,8 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
-// import com.google.cloud.datastore;
-// import com.google.cloud.datastore.Query;
-// import com.google.cloud.datastore.StructuredQuery;
 import com.google.sps.data.Comments.CommentObject;
 import com.google.sps.data.CommentAdapter;
-// import com.google.sps.data.CommentAdapter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -41,13 +37,10 @@ public class DataServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        System.out.println("get");
-        // Query<Entity> query = Query.newEntityQueryBuilder().setKind("Comment").setOrderBy(OrderBy.desc("time")).setLimit(Integer.parseInt(request.getParameter("num-comments"))).build();
         Query query = new Query("Comment").addSort("time", SortDirection.DESCENDING);
 
         DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         PreparedQuery results = datastore.prepare(query);
-        // QueryResults<Entity> results = datastore.run(query);
 
         Integer numComments = Integer.parseInt(request.getParameter("num-comments"));
         CommentObject.Builder commentBuilder;
@@ -56,8 +49,6 @@ public class DataServlet extends HttpServlet {
         for (Entity entity : results.asIterable()) {
             if (numComments > 0) {
                 long id = entity.getKey().getId();
-                // String comment = entity.getProperty("commentObject").toString();
-
                 String text = (String) entity.getProperty("text");
                 long timestamp = (long) entity.getProperty("time");
 
@@ -66,15 +57,11 @@ public class DataServlet extends HttpServlet {
                 commentBuilder.setText(text);
                 commentBuilder.setTime(timestamp);
                 CommentObject hold = commentBuilder.build();
-                System.out.println(hold.getText());
 
-                // Comment comment = new Comment(id, text, timestamp);
                 comments.add(hold);
                 numComments--;
             }
         }
-
-        // System.out.println(comments.size() + "\n");
 
         GsonBuilder gsonBuilder = new GsonBuilder();
         Gson gson = gsonBuilder.registerTypeAdapter(CommentObject.class, new CommentAdapter()).create();
@@ -85,7 +72,6 @@ public class DataServlet extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        System.out.println("post");
         String comment = request.getParameter("comment-text");
         System.out.println(comment);
         long timestamp = System.currentTimeMillis();
@@ -96,17 +82,10 @@ public class DataServlet extends HttpServlet {
             newComment.setProperty("text", comment);
             newComment.setProperty("time", timestamp);
 
-            // Comments.CommentObject commentObject = Comments.CommentObject.newBuilder().setText(comment).setTime(timestamp).build();
-
-            // newComment.setProperty("text", commentObject.getText());
-            // newComment.setProperty("time", commentObject.getTime());
-
-            System.out.println(newComment.getProperty("text"));
-
             DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
             datastore.put(newComment);
         }
 
-        response.sendRedirect("/data");
+        response.sendRedirect("/index.html");
     }
 }
